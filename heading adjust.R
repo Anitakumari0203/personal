98a8baa27371bcd78cc0ca1f406f00496dd0c556
Anitakumari0203 <68773643+Anitakumari0203@users.noreply.github.com>
2020-08-05 20:11:22 +0530

    parameter adjust to family

---
+++ heading adjust.R
@@ -0,0 +1,9 @@
+#adj- specifies the position of the test(0-1,.5=center=defult)
+x<-1:5
+plot(x,main="testplot",ask=1)
+plot(x,ann=t)
+par(bg="white")
+plot(x)
+plot(x,bty="n")
+plot(x,main="plot")
+plot(x,cex=5,main="plot",cex.main=5)
